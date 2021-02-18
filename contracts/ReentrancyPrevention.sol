284519cd59c60bbf82bbb9dbeadf7c7eeeedeea5
ysfkel <yusufkel@gmail.com>
2021-02-19 01:08:22 +0400

    added prevention

--- contracts/ReentrancyPrevention.sol
+++ contracts/ReentrancyPrevention.sol
@@ -3,11 +3,27 @@ pragma solidity 0.7.5;
 contract PreventReentrancy{
 
     bool reEntrancyMutext = false ;
-    uint256 public withdrawalKimit = 1 ether;
+    uint256 public withdrawalLimit = 1 ether;
     mapping(address => uint256) public lastWithdrawalTime; 
     mapping(address => uint256) public balances; 
 
     function depositFunds() public payable {
         balances[msg.sender] += msg.value;
+    }
+
+    function withdrawFunds(uint _weiToWithdraw) public {
+        require(!reEntrancyMutext);
+        require(balances[msg.sender] >= _weiToWithdraw);
+
+        require(_weiToWithdraw <= withdrawalLimit);
+
+        require(block.timestamp >= lastWithdrawalTime[msg.sender] + 1 weeks);
+
+        balances[msg.sender] -= _weiToWithdraw;
+        lastWithdrawalTime[msg.sender] = block.timestamp;
+
+        reEntrancyMutext = true;
+        msg.sender.transfer(_weiToWithdraw);
+        reEntrancyMutext = false;
     }
 }
